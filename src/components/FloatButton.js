809b4d9a6a60715aca2c8b904077ac4c2e27fdf4
chungune <ja246@naver.com>
2022-01-05 10:22:10 +0900

    test

--- src/components/FloatButton.js
+++ src/components/FloatButton.js
@@ -1,5 +1,8 @@
 import React from "react";
+
+import styled from "styled-components";
 import { Grid, Button } from "../elements/index";
+import write from "../assets/freeBoard/write_icon.png"
 
 const FloatButton = (props) => {
   const {
@@ -8,13 +11,22 @@ const FloatButton = (props) => {
 
   return (
     <React.Fragment>
-      <Grid align="end">
+      <Float>
         <Button floatBtn _onClick={_onClick}>+</Button>
-      </Grid>
+      </Float>
     </React.Fragment>
   );
 };
 
-
+const Float = styled.div`
+  position: absolute;
+  bottom: 16px;
+  right: 16px;
+  width: 53px;
+  height: 53px;
+  border-radius: 999px;
+  background: url(${write}) no-repeat center;
+  border: 1px solid red;
+`
 
 export default FloatButton;
